36855ac65ef22a21fecf044aa892f9de94686dab
JongJae Park <pjj186@naver.com>
2023-01-16 00:59:34 +0900

    Prisma Client

--- server.ts
+++ server.ts
@@ -1,40 +1,56 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
+
+interface ICreateMovieData {
+  title: string;
+  year: number;
+  genre?: string;
+}
 
 // The GraphQL schema
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   # 쿼리
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id: Int!): Movie
   }
   # 뮤테이션
   type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    movies: () => [],
-    movie: () => ({
+    movies: () => client.movie.findMany(),
+    movie: (_: any, { id }: any) => ({
       title: "Iron Man",
       year: 2004,
     }),
   },
   Mutation: {
-    createMovie: (_: any, args: any) => {
-      console.log(args);
-      return true;
-    },
-    deleteMovie: (_: any, args: any) => {
-      console.log(args);
+    createMovie: (_: any, { title, year, genre }: ICreateMovieData) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_: any, { id }: any) => {
+      console.log(id);
       return true;
     },
   },
