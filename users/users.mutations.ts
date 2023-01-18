cf04a075089c44c3f8115849d453ae3d6e8c1c54
JongJae Park <pjj186@naver.com>
2023-01-19 00:01:48 +0900

    login

--- users/users.mutations.ts
+++ users/users.mutations.ts
@@ -38,5 +38,25 @@ export default {
         return e;
       }
     },
+    login: async (_: any, { username, password }: IAccount) => {
+      const user = await client.user.findFirst({
+        where: {
+          username,
+        },
+      });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user?.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password",
+        };
+      }
+    },
   },
 };
