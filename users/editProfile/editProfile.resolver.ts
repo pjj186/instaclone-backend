f2a31e93533da28b6ea536be06ab77f9cb861220
JongJae Park <pjj186@naver.com>
2023-01-23 00:44:17 +0900

    currying

--- users/editProfile/editProfile.resolver.ts
+++ users/editProfile/editProfile.resolver.ts
@@ -4,47 +4,41 @@ import bcrypt from "bcrypt";
 import { IContext } from "../../server";
 import { protectedResolver } from "../users.utils";
 
+const resolverFn = async (
+  _: any,
+  { firstName, username, lastName, email, password: newPassword }: IAccount,
+  { loggedInUser }: IContext
+) => {
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "프로필을 업데이트 할 수 없습니다.",
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _: any,
-        {
-          firstName,
-          username,
-          lastName,
-          email,
-          password: newPassword,
-        }: IAccount,
-        { loggedInUser }: IContext
-      ) => {
-        let uglyPassword = null;
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(uglyPassword && { password: uglyPassword }),
-          },
-        });
-        if (updatedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "프로필을 업데이트 할 수 없습니다.",
-          };
-        }
-      }
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
