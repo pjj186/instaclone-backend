ba9931a9f6d76fb6df006a99b6d19e1b35ef410b
JongJae Park <pjj186@naver.com>
2023-01-25 06:32:46 +0900

    Ejecting from Apollo Server

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
+import { ApolloServer } from "apollo-server-express";
 dotenv.config();
-import { ApolloServer } from "apollo-server";
 import { typeDefs, resolvers } from "./schema";
 import { ExpressContext } from "apollo-server-express/src/ApolloServer";
 import { getUser } from "./users/users.utils";
 import { User } from "@prisma/client";
+import logger from "morgan";
+import express from "express";
 
 export interface IContext {
   loggedInUser: User;
@@ -13,16 +15,25 @@ export interface IContext {
 
 const PORT = process.env.PORT;
 
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-  context: async ({ req }: ExpressContext) => {
-    return {
-      loggedInUser: await getUser(req?.headers?.authorization!),
-    };
-  },
-});
+const startServer = async () => {
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    context: async ({ req }: ExpressContext) => {
+      return {
+        loggedInUser: await getUser(req?.headers?.authorization!),
+      };
+    },
+  });
 
-server.listen(PORT).then(() => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}/`);
-});
+  const app = express(); // create express server
+  app.use(logger("tiny"));
+  await server.start();
+  server.applyMiddleware({ app }); // apollo server with express server
+
+  app.listen({ port: PORT }, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+  });
+};
+
+startServer();
