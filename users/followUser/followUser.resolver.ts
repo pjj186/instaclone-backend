d68f83965eedc3881dcc7d16a7c27861689824fb
JongJae Park <pjj186@naver.com>
2023-01-25 20:05:12 +0900

    Following User

---
+++ users/followUser/followUser.resolver.ts
@@ -0,0 +1,41 @@
+import client from "../../client";
+import { IContext } from "../../server";
+import { IAccount } from "../users.types";
+import { protectedResolver } from "../users.utils";
+
+const resolverFn = async (
+  _: any,
+  { username }: IAccount,
+  { loggedInUser }: IContext
+) => {
+  const ok = await client.user.findUnique({
+    where: { username },
+  });
+  if (!ok) {
+    return {
+      ok: false,
+      error: "존재하지 않는 유저입니다.",
+    };
+  }
+  await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      following: {
+        connect: {
+          username,
+        },
+      },
+    },
+  });
+  return {
+    ok: true,
+  };
+};
+
+export default {
+  Mutation: {
+    followUser: protectedResolver(resolverFn),
+  },
+};
