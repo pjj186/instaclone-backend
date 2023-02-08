42dae01c2f35188b2dc3f40d7092483a9c70af94
JongJae Park <pjj186@naver.com>
2023-02-08 15:04:39 +0900

    seeFeed

---
+++ photos/seeFeed/seeFeed.resolver.ts
@@ -0,0 +1,32 @@
+import client from "../../client";
+import { Context } from "../../users/types";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolver(
+      async (_: any, __: any, { loggedInUser }: Context) =>
+        client.photo.findMany({
+          where: {
+            OR: [
+              {
+                user: {
+                  followers: {
+                    some: {
+                      id: loggedInUser?.id,
+                    },
+                  },
+                },
+              },
+              {
+                userId: loggedInUser?.id,
+              },
+            ],
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        })
+    ),
+  },
+};
