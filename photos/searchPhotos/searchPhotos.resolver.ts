682f115bbf35611dcdf9f185a1083e1591013503
JongJae Park <pjj186@naver.com>
2023-02-08 11:36:09 +0900

    searchPhotos pagination

--- photos/searchPhotos/searchPhotos.resolver.ts
+++ photos/searchPhotos/searchPhotos.resolver.ts
@@ -2,17 +2,20 @@ import client from "../../client";
 
 interface searchPhotosParams {
   keyword: string;
+  page: number;
 }
 
 export default {
   Query: {
-    searchPhotos: (_: any, { keyword }: searchPhotosParams) =>
+    searchPhotos: (_: any, { keyword, page }: searchPhotosParams) =>
       client.photo.findMany({
         where: {
           caption: {
             startsWith: keyword,
           },
         },
+        take: 5,
+        skip: (page - 1) * 5,
       }),
   },
 };
