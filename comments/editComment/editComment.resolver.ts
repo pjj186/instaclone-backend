5f40f19daeb98c61f3c54b0a7a03cd8850d4490f
JongJae Park <pjj186@naver.com>
2023-02-08 17:21:26 +0900

    editComment + MutationResponse

---
+++ comments/editComment/editComment.resolver.ts
@@ -0,0 +1,52 @@
+import client from "../../client";
+import { Context } from "../../users/types";
+import { protectedResolver } from "../../users/users.utils";
+
+interface editCommentParams {
+  id: number;
+  payload: string;
+}
+
+export default {
+  Mutation: {
+    editComment: protectedResolver(
+      async (
+        _: any,
+        { id, payload }: editCommentParams,
+        { loggedInUser }: Context
+      ) => {
+        const comment = await client.comment.findUnique({
+          where: {
+            id,
+          },
+          select: {
+            userId: true,
+          },
+        });
+        if (!comment) {
+          return {
+            ok: false,
+            error: "comment not found",
+          };
+        } else if (comment.userId !== loggedInUser?.id) {
+          return {
+            ok: false,
+            error: "not authorized",
+          };
+        } else {
+          await client.comment.update({
+            where: {
+              id,
+            },
+            data: {
+              payload,
+            },
+          });
+          return {
+            ok: true,
+          };
+        }
+      }
+    ),
+  },
+};
