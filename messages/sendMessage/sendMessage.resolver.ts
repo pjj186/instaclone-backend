77b57d8e63a2c82d45c48d9ae74a5fa0f9600e11
JongJae Park <pjj186@naver.com>
2023-02-13 00:59:51 +0900

    Update sendMessage.resolver.ts

--- messages/sendMessage/sendMessage.resolver.ts
+++ messages/sendMessage/sendMessage.resolver.ts
@@ -2,6 +2,15 @@ import client from "../../client";
 import { Context } from "../../users/types";
 import { protectedResolver } from "../../users/users.utils";
 
+/*
+# 처음 상대방에게 메세지를 보낼 때
+userId만 받아서 새로운 room모델을 생성하고, 그 room모델에 로그인한 유저와 메세지를 보낸 유저를 연결시켜준다.
+새로운 message모델을 생성하고, 로그인한 유저와 생성한 room모델을 연결시켜준다.
+
+# 이미 room이 존재할 때 (첫 메세지가 아닌 두 번째 메세지부터)
+userId가 아닌 roomId만 받고, 새로운 message모델을 생성하고, 로그인한 유저와 기존에 존재하는 room모델을 연결시켜준다. 
+*/
+
 interface sendMessageParams {
   payload: string;
   roomId: number;
