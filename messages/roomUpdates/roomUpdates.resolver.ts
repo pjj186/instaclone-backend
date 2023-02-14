07030f0bed403b5b257fede87e231eaa44bfcce9
JongJae Park <pjj186@naver.com>
2023-02-14 15:21:22 +0900

    Filtering Subscriptions

--- messages/roomUpdates/roomUpdates.resolver.ts
+++ messages/roomUpdates/roomUpdates.resolver.ts
@@ -1,10 +1,21 @@
+import { Message } from "@prisma/client";
+import { withFilter } from "apollo-server-express";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
+
+interface RoomUpdatesPayload {
+  roomUpdates: Message;
+}
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(NEW_MESSAGE),
+        ({ roomUpdates }: RoomUpdatesPayload, { id }) => {
+          return roomUpdates.roomId === id;
+        }
+      ),
     },
   },
 };
