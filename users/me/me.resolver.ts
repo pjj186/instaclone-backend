2dcbd44c80c42ebf42fa96d7fc84ee04d7b40423
JongJae Park <pjj186@naver.com>
2023-04-20 12:47:48 +0900

    Update me.resolver.ts

--- users/me/me.resolver.ts
+++ users/me/me.resolver.ts
@@ -2,14 +2,16 @@ import client from '../../client';
 import { Context } from '../types';
 import { protectedResolver } from '../users.utils';
 
+const resolverFn = async (_: any, __: any, { loggedInUser }: Context) => {
+  return client.user.findUnique({
+    where: {
+      id: loggedInUser?.id,
+    },
+  });
+};
+
 export default {
   Query: {
-    me: protectedResolver(async (_: any, __: any, { loggedInUser }: Context) =>
-      client.user.findUnique({
-        where: {
-          id: loggedInUser?.id,
-        },
-      }),
-    ),
+    me: protectedResolver(resolverFn),
   },
 };
