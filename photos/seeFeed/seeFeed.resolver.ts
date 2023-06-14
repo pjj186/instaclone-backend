81d903bb66344dacdfeff3f3f3d7de9f1f02d1e3
jongjaepark <pjj186@naver.com>
2023-06-14 12:04:01 +0900

    Infinite Scrolling

--- photos/seeFeed/seeFeed.resolver.ts
+++ photos/seeFeed/seeFeed.resolver.ts
@@ -1,12 +1,15 @@
 import client from '../../client';
 import { Context } from '../../users/types';
 import { protectedResolver } from '../../users/users.utils';
+import { IFeedParams } from '../photos.types';
 
 export default {
   Query: {
     seeFeed: protectedResolver(
-      async (_: any, __: any, { loggedInUser }: Context) =>
+      async (_: any, { offset }: IFeedParams, { loggedInUser }: Context) =>
         client.photo.findMany({
+          take: 2,
+          skip: offset,
           where: {
             OR: [
               {
--- photos/seeFeed/seeFeed.typeDefs.ts
+++ photos/seeFeed/seeFeed.typeDefs.ts
@@ -1,7 +1,7 @@
-import { gql } from "apollo-server-express";
+import { gql } from 'apollo-server-express';
 
 export default gql`
   type Query {
-    seeFeed: [Photo]
+    seeFeed(offset: Int!): [Photo]
   }
 `;
